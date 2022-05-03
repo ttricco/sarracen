653345b817450a602ebf81415f61a289139842d1
Terrence Tricco <tstricco@mun.ca>
2022-05-03 14:32:13 -0230

    Better label and unit parsing for read_csv and unit tests

--- sarracen/io/read_csv.py
+++ sarracen/io/read_csv.py
@@ -1,7 +1,20 @@
 import pandas as pd
-
 from sarracen.sarracen_dataframe import SarracenDataFrame
 
+
+def _get_units(columns):
+     return columns.str.extract(r'((?<=\[).+(?=\]))')[0]
+
+def _get_labels(columns):
+     return columns.str.extract(r'(^[^\[]*[^\s\[])')[0]
+
 def read_csv(filename):
-    df = pd.read_csv(filename)
-    return SarracenDataFrame(df)
+    df = SarracenDataFrame(pd.read_csv(filename))
+
+    labels = _get_labels(df.columns)
+    units = _get_units(df.columns)
+
+    #df.columns = labels
+    #df.units = units
+
+    return df
