eeb27055d9e5269866775efb3467595cb1ac829a
Terrence Tricco <tstricco@mun.ca>
2022-05-03 15:18:50 -0230

    Better params handling in SarracenDataFrame plus initial units code

--- sarracen/io/read_csv.py
+++ sarracen/io/read_csv.py
@@ -2,13 +2,19 @@ import pandas as pd
 from sarracen.sarracen_dataframe import SarracenDataFrame
 
 
-def _get_units(columns):
-     return columns.str.extract(r'((?<=\[).+(?=\]))')[0]
-
-def _get_labels(columns):
-     return columns.str.extract(r'(^[^\[]*[^\s\[])')[0]
-
 def read_csv(filename):
+    """
+    Read data from a csv file.
+
+    Parameters
+    ----------
+    filename : str
+        Name of the file to be loaded.
+
+    Returns
+    -------
+    SarracenDataFrame
+    """
     df = SarracenDataFrame(pd.read_csv(filename))
 
     labels = _get_labels(df.columns)
@@ -18,3 +24,13 @@ def read_csv(filename):
     #df.units = units
 
     return df
+
+
+
+
+def _get_units(columns):
+     return columns.str.extract(r'((?<=\[).+(?=\]))')[0]
+
+
+def _get_labels(columns):
+     return columns.str.extract(r'(^[^\[]*[^\s\[])')[0]
