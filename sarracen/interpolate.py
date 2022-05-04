13a4671657e787fe35cd833393c68a24c8d71e5b
Andrew Harris <arharris@mun.ca>
2022-05-04 12:16:04 -0230

    Adjust normalization and rounding for 2D interpolation

--- sarracen/interpolate.py
+++ sarracen/interpolate.py
@@ -19,7 +19,7 @@ def interpolate2D(data, x, y, target, kernel, xmin, ymin, pixwidthx, pixwidthy, 
     :param pixwidthy: The height that each pixel represents in particle data space
     :return: The output image, in a 2-dimensional numpy array.
     """
-    image = np.zeros((pixcountx, pixcounty))
+    image = np.zeros((pixcounty, pixcountx))
 
     # iterate through all pixels
     for i, particle in data.iterrows():
@@ -27,14 +27,12 @@ def interpolate2D(data, x, y, target, kernel, xmin, ymin, pixwidthx, pixwidthy, 
         # w_i = m_i / (rho_i * (h_i) ** 2)
         weight = particle['m'] / (particle['rho'] * particle['h'] ** 2)
 
-        # normalize the weight using the kernel normalization constant
-        termnorm = kernel.cnormk2D * weight
         # skip particles with 0 weight
-        if termnorm <= 0: continue
+        if weight <= 0: continue
 
         # kernel radius scaled by the particle's 'h' value
         radkern = kernel.radkernel * particle['h']
-        term = termnorm * particle[target]
+        term = weight * particle[target]
         hi1 = 1 / particle['h']
         hi21 = hi1 ** 2
 
@@ -42,10 +40,10 @@ def interpolate2D(data, x, y, target, kernel, xmin, ymin, pixwidthx, pixwidthy, 
         part_y = particle[y]
 
         # determine the min/max x&y coordinates affected by this particle
-        ipixmin = int((part_x - radkern - xmin)/np.abs(pixwidthx))
-        jpixmin = int((part_y - radkern - ymin)/np.abs(pixwidthy))
-        ipixmax = int((part_x + radkern - xmin)/np.abs(pixwidthx))
-        jpixmax = int((part_y + radkern - ymin)/np.abs(pixwidthy))
+        ipixmin = int(np.rint((part_x - radkern - xmin)/np.abs(pixwidthx)))
+        jpixmin = int(np.rint((part_y - radkern - ymin)/np.abs(pixwidthy)))
+        ipixmax = int(np.rint((part_x + radkern - xmin)/np.abs(pixwidthx)))
+        jpixmax = int(np.rint((part_y + radkern - ymin)/np.abs(pixwidthy)))
 
         # ensure that the min/max x&y coordinates remain within the bounds of the image
         if ipixmin < 0: ipixmin = 0
@@ -56,21 +54,21 @@ def interpolate2D(data, x, y, target, kernel, xmin, ymin, pixwidthx, pixwidthy, 
         # precalculate derivatives in the x-direction (optimization)
         dx2i = np.zeros(pixcountx)
         for ipix in range(ipixmin, ipixmax):
-            dx2i[ipix] = ((xmin + (ipix - 0.5) * pixwidthx - part_x) ** 2) * hi21
+            dx2i[ipix] = ((xmin + (ipix + 0.5) * pixwidthx - part_x) ** 2) * hi21
 
         # traverse horizontally through affected pixels
         for jpix in range(jpixmin, jpixmax):
             # determine derivatives in the y-direction
-            ypix = ymin + (jpix - 0.5) * pixwidthy
+            ypix = ymin + (jpix + 0.5) * pixwidthy
             dy = ypix - part_y
             dy2 = dy * dy * hi21
 
             for ipix in range(ipixmin, ipixmax):
                 # calculate contribution at i, j due to particle at x, y
                 q2 = dx2i[ipix] + dy2
-                wab = kernel.w(q2, 2)
+                wab = kernel.w(np.sqrt(q2), 2)
 
                 # add contribution to image
-                image[ipix][jpix] += term * wab
+                image[jpix][ipix] += term * wab
 
     return image
