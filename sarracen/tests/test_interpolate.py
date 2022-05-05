e76020a9f3f0af157df79c8b266bc4e8cd3fb7a5
Andrew Harris <arharris@mun.ca>
2022-05-05 14:36:29 -0230

    Apply Interpolate2d requested changes

--- sarracen/tests/test_interpolate.py
+++ sarracen/tests/test_interpolate.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from pytest import approx
 
 from sarracen import SarracenDataFrame
@@ -15,10 +16,9 @@ def test_interpolate2d():
                        'm': [1]})
     sdf = SarracenDataFrame(df, params=dict())
 
-    image = interpolate2D(sdf, 'x', 'y', 'P', CubicSplineKernel(), -2, -2, 0.1, 0.1, 40, 40)
+    image = interpolate2D(sdf, 'x', 'y', 'P', CubicSplineKernel(), 0.1, 0.1, -2, -2, 40, 40)
 
     assert image[0][0] == 0
-    # sqrt((-1.95)^2+(0.05)^2)
-    assert image[20][0] == approx(CubicSplineKernel().w(1.95064, 2), rel=1e-4)
-    # sqrt((0.05)^2+(0.05)^2)
-    assert image[20][20] == approx(CubicSplineKernel().w(0.070711, 2), rel=1e-4)
+    assert image[20][0] == approx(CubicSplineKernel().w(np.sqrt((-1.95) ** 2 + 0.05 ** 2), 2), rel=1e-8)
+    assert image[20][20] == approx(CubicSplineKernel().w(np.sqrt(0.05 ** 2 + 0.05 ** 2), 2), rel=1e-8)
+    assert image[12][17] == approx(CubicSplineKernel().w(np.sqrt(0.75 ** 2 + 0.25 ** 2), 2), rel=1e-8)
