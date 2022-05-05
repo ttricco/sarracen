4f031115c6d734fbfef5791d4fdf737117961a46
Andrew Harris <arharris@mun.ca>
2022-05-05 16:42:11 -0230

    Refactor Kernel Dimensionality

--- sarracen/kernels/base_kernel.py
+++ sarracen/kernels/base_kernel.py
@@ -1,24 +1,27 @@
+from typing import Callable
+
+
 class BaseKernel:
     """A generic kernel used for data interpolation."""
-    def __init__(self, radkernel, cnormk1d, cnormk2d, cnormk3d, wfunc):
+    def __init__(self, radkernel: float, ndims: int, cnormk: float, wfunc: Callable):
+        if ndims < 1 or ndims > 3:
+            raise ValueError('Invalid Number of Dimensions!')
+
         self._radkernel = radkernel
-        self._cnormk1d = cnormk1d
-        self._cnormk2d = cnormk2d
-        self._cnormk3d = cnormk3d
+        self._ndims = ndims
+        self._cnormk = cnormk
         self._wfunc = wfunc
 
-    def w(self, q, dimension):
+    def w(self, q: float) -> float:
         """
         The dimensionless part of this kernel at a specific value of q.
         :param q: The value of q to evaluate this kernel at.
-        :param dimension: The number of dimensions (for normalization).
         :return:
         """
-        norm = self._cnormk1d if (dimension == 1) else \
-            self._cnormk2d if (dimension == 2) else \
-            self._cnormk3d
+        if q < 0:
+            raise ValueError('q cannot be lower than 0.')
 
-        return norm * self._wfunc(q)
+        return self._cnormk * self._wfunc(q)
 
     @property
     def radkernel(self):
@@ -29,25 +32,17 @@ class BaseKernel:
         return self._radkernel
 
     @property
-    def cnormk1d(self):
-        return self._cnormk1d
+    def cnormk(self):
+        return self._cnormk
 
-    @cnormk1d.getter
-    def cnormk1d(self):
-        return self._cnormk1d
-
-    @property
-    def cnormk2d(self):
-        return self._cnormk2d
-
-    @cnormk2d.getter
-    def cnormk2d(self):
-        return self._cnormk2d
+    @cnormk.getter
+    def cnormk(self):
+        return self._cnormk
 
     @property
-    def cnormk3d(self):
-        return self._cnormk3d
+    def ndims(self):
+        return self._ndims
 
-    @cnormk3d.getter
-    def cnormk3d(self):
-        return self._cnormk3d
+    @ndims.getter
+    def ndims(self):
+        return self._ndims
--- sarracen/kernels/quartic_spline.py
+++ sarracen/kernels/quartic_spline.py
@@ -7,16 +7,21 @@ class QuarticSplineKernel(BaseKernel):
     """
     An implementation of the Quartic Spline kernel, in 1, 2, and 3 dimensions.
     """
-    def __init__(self):
-        super().__init__(2.5, 1/24, 96/(1199*np.pi), 1/(20*np.pi), self._weight)
+
+    def __init__(self, ndims):
+        norm = 1 / 24 if (ndims == 1) else \
+                96 / (1199 * np.pi) if (ndims == 2) else \
+                1 / (20 * np.pi)
+
+        super().__init__(2.5, ndims, norm, self._weight)
 
     @staticmethod
     def _weight(q):
         if q < 0.5:
-            return ((5/2) - q)**4 - 5*((3/2) - q)**4 + 10*((1/2) - q)**4
+            return ((5 / 2) - q) ** 4 - 5 * ((3 / 2) - q) ** 4 + 10 * ((1 / 2) - q) ** 4
         elif q < 1.5:
-            return ((5/2) - q)**4 - 5*((3/2) - q)**4
+            return ((5 / 2) - q) ** 4 - 5 * ((3 / 2) - q) ** 4
         elif q < 2.5:
-            return ((5/2) - q)**4
+            return ((5 / 2) - q) ** 4
         else:
             return 0
