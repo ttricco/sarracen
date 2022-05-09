aa88f98139b8b017e7e3dad8fd786dc3941a35d1
Terrence Tricco <tstricco@mun.ca>
2022-05-09 16:30:26 -0230

    Add Phantom file reading

--- sarracen/__init__.py
+++ sarracen/__init__.py
@@ -1,5 +1,6 @@
 
 from sarracen.io.read_marisa import read_marisa
 from sarracen.io.read_csv import read_csv
+from sarracen.io.read_phantom import read_phantom
 
 from sarracen.sarracen_dataframe import SarracenDataFrame
