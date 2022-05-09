aeee5881a6040d14e779add0743bf624ad7f71b9
Andrew Harris <arharris@mun.ca>
2022-05-09 12:32:50 -0230

    Initial implementation of 1D cross-sections

--- sarracen/interpolate.py
+++ sarracen/interpolate.py
@@ -46,7 +46,7 @@ def interpolate2D(data: SarracenDataFrame,
 
     image = np.zeros((pixcounty, pixcountx))
 
-    # iterate through all pixels
+    # iterate through all particles
     for i, particle in data.iterrows():
         # dimensionless weight
         # w_i = m_i / (rho_i * (h_i) ** 2)
@@ -102,3 +102,96 @@ def interpolate2D(data: SarracenDataFrame,
                 image[jpix][ipix] += term * wab
 
     return image
+
+
+def interpolate2DCross(data: SarracenDataFrame,
+                       x: str,
+                       y: str,
+                       target: str,
+                       kernel: BaseKernel,
+                       x1: float,
+                       y1: float,
+                       x2: float,
+                       y2: float,
+                       pixcount: int):
+    if np.isclose(y2, y1) and np.isclose(x2, x1):
+        raise ValueError('Zero length cross section!')
+
+    if pixcount <= 0:
+        raise ValueError('pixcount must be greater than zero!')
+
+    if kernel.ndims != 2:
+        raise ValueError("Kernel must be two-dimensional!")
+
+    output = np.zeros(pixcount)
+
+    gradient = 0
+    if not np.isclose(x2, x1):
+        gradient = (y2 - y1) / (x2 - x1)
+    yint = y2 - gradient * x2
+
+    xlength = np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    pixwidth = xlength / pixcount
+    xpixwidth = (x2 - x1) / pixcount
+
+    # iterate through all particles
+    for i, particle in data.iterrows():
+        # dimensionless weight
+        # w_i = m_i / (rho_i * (h_i) ** 2)
+        weight = particle['m'] / (particle['rho'] * particle['h'] ** 2)
+
+        if weight <= 0:
+            continue
+
+        radkern = kernel.radkernel * particle['h']
+        term = weight * particle[target]
+        hi1 = 1 / particle['h']
+
+        aa = 1 + gradient**2
+        bb = 2 * gradient * (yint - particle[y]) - 2 * particle[x]
+        cc = particle[x] ** 2 - particle[y] ** 2 - 2 * yint * particle[y] + yint ** 2 - radkern**2
+
+        determinant = bb**2 - 4*aa*cc
+        if determinant > 0:
+            det = np.sqrt(determinant)
+            xstart = (-bb - det) / (2 * aa)
+            xend = (-bb + det) / (2 * aa)
+
+            if xstart < x1:
+                xstart = x1
+            if xstart > x2:
+                xstart = x2
+            if xend < x1:
+                xend = x1
+            if xend > x2:
+                xend = x2
+
+            ystart = gradient*xstart + yint
+            yend = gradient*xend + yint
+
+            rstart = np.sqrt((xstart-x1)**2 + (ystart-y1)**2)
+            rend = np.sqrt((xend-x1)**2 + (yend-y1)**2)
+
+            ipixmin = int(np.rint(rstart/pixwidth))
+            ipixmax = int(np.rint(rend/pixwidth))
+
+            if ipixmin < 0:
+                ipixmin = 0
+            if ipixmax < 0:
+                ipixmax = 0
+            if ipixmin > pixcount:
+                ipixmin = pixcount
+            if ipixmax > pixcount:
+                ipixmax = pixcount
+
+            for ipix in range(ipixmin, ipixmax):
+                xpix = x1 + (ipix+0.5)*xpixwidth
+                ypix = gradient*xpix + yint
+                dy = ypix - particle[y]
+                dx = xpix - particle[x]
+                q2 = (dx*dx + dy*dy)*hi1*hi1
+
+                wab = kernel.w(np.sqrt(q2))
+                output[ipix] += term * wab
+
+    return output
