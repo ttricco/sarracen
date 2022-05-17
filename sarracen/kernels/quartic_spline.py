088c1f387dec425bb61e517b945135c5d278b7a4
Andrew Harris <arharris@mun.ca>
2022-05-17 12:58:32 -0230

    Initial numba integration

--- sarracen/kernels/quartic_spline.py
+++ sarracen/kernels/quartic_spline.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numba import jit
 
 from sarracen.kernels import BaseKernel
 
@@ -8,13 +9,16 @@ class QuarticSplineKernel(BaseKernel):
     An implementation of the Quartic Spline kernel, in 1, 2, and 3 dimensions.
     """
 
-    def __init__(self, ndims):
-        norm = 1 / 24 if (ndims == 1) else \
-                96 / (1199 * np.pi) if (ndims == 2) else \
-                1 / (20 * np.pi)
-
-        super().__init__(2.5, ndims, norm, self._weight)
+    @staticmethod
+    def get_radius() -> float:
+        return 2.5
 
     @staticmethod
-    def _weight(q):
-        return ((5 / 2) - q) ** 4 * (q < 2.5) - 5 * ((3 / 2) - q) ** 4 * (q < 1.5) + 10 * ((1 / 2) - q) ** 4 * (q < 0.5)
+    @jit(nopython=True, nogil=True, parallel=True, fastmath=True)
+    def w(q: float, ndim: int):
+        norm = 1 / 24 if (ndim == 1) else \
+            96 / (1199 * np.pi) if (ndim == 2) else \
+            1 / (20 * np.pi)
+
+        return norm * (((5 / 2) - q) ** 4 * (q < 2.5) - 5 * ((3 / 2) - q) ** 4 * (q < 1.5) + 10 * ((1 / 2) - q) ** 4 * (
+                    q < 0.5))
--- sarracen/kernels/quintic_spline.py
+++ sarracen/kernels/quintic_spline.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numba import jit
 
 from sarracen.kernels import BaseKernel
 
@@ -8,13 +9,15 @@ class QuinticSplineKernel(BaseKernel):
     An implementation of the Quintic Spline kernel, in 1, 2, and 3 dimensions.
     """
 
-    def __init__(self, ndims):
-        norm = 1 / 120 if (ndims == 1) else \
-                7 / (478 * np.pi) if (ndims == 2) else \
-                1 / (120 * np.pi)
-
-        super().__init__(3.0, ndims, norm, self._weight)
+    @staticmethod
+    def get_radius() -> float:
+        return 3
 
     @staticmethod
-    def _weight(q):
-        return (3 - q) ** 5 * (q < 3) - 6 * (2 - q) ** 5 * (q < 2) + 15 * (1 - q) ** 5 * (q < 1)
+    @jit(nopython=True, nogil=True, parallel=True, fastmath=True)
+    def w(q: float, ndim: int):
+        norm = 1 / 120 if (ndim == 1) else \
+            7 / (478 * np.pi) if (ndim == 2) else \
+            1 / (120 * np.pi)
+
+        return norm * ((3 - q) ** 5 * (q < 3) - 6 * (2 - q) ** 5 * (q < 2) + 15 * (1 - q) ** 5 * (q < 1))
