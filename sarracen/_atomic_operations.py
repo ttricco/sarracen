85b7ee6c41c21e2a490ef1b2be5c89fbd097f4a0
Andrew Harris <arharris@mun.ca>
2022-06-09 14:09:03 -0230

    Add license and attribution to _atomic_operations.py

--- sarracen/_atomic_operations.py
+++ sarracen/_atomic_operations.py
@@ -1,5 +1,14 @@
-"""
-https://github.com/KatanaGraph/katana/blob/master/python/katana/numba_support/numpy_atomic.py
+# The 3-Clause BSD License
+# Copyright 2018 The University of Texas at Austin
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+""" Various atomic array operations, compatible in numba multithreaded contexts.
+
+Sourced From:
+https://github.com/KatanaGraph/katana/blob/fa1a1a649bdd65e62db8cc499bdc194176159c78/python/katana/native_interfacing/numpy_atomic.py
 """
 
 from numba import types
