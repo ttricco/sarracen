0e3f0ed4f79a2a5c2f67730912c5ecd65fe75fc5
Andrew Harris <arharris@mun.ca>
2022-07-05 15:18:18 -0230

    Create __init__ file for backends

---
+++ sarracen/backends/__init__.py
@@ -0,0 +1,4 @@
+from sarracen.backends.base_backend import BaseBackend
+from sarracen.backends.cpu_backend import CPUBackend
+from sarracen.backends.gpu_backend import GPUBackend
+from sarracen.backends.cupy_backend import CuPyBackend
