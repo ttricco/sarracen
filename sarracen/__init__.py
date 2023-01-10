1f831b190505f22bd8a195051e92cf68604279c1
Terrence Tricco <tstricco@mun.ca>
2023-01-10 09:35:04 -0330

    use relative imports for sarracen files

--- sarracen/__init__.py
+++ sarracen/__init__.py
@@ -1,9 +1,12 @@
 
-from sarracen.readers.read_marisa import read_marisa
-from sarracen.readers.read_csv import read_csv
-from sarracen.readers.read_phantom import read_phantom
+from .readers.read_marisa import read_marisa
+from .readers.read_csv import read_csv
+from .readers.read_phantom import read_phantom
 
-from sarracen.sarracen_dataframe import SarracenDataFrame
+from .sarracen_dataframe import SarracenDataFrame
 
-from sarracen.interpolate import interpolate_2d, interpolate_2d_line, interpolate_3d, interpolate_3d_cross
-from sarracen.render import render, streamlines, arrowplot
+from .interpolate import interpolate_2d, interpolate_2d_line, interpolate_3d, interpolate_3d_cross
+from .render import render, streamlines, arrowplot
+
+
+__version__ = "1.0"
