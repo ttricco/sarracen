3c5c6b57f012b6746e353f445c6a982fe65f348a
Terrence Tricco <tstricco@mun.ca>
2023-12-19 10:45:56 -0330

    read_phantom creates mass column if multiple species mixed in a single dataframe

--- sarracen/readers/read_phantom.py
+++ sarracen/readers/read_phantom.py
@@ -186,6 +186,17 @@ def _read_array_blocks(fp, def_int_dtype, def_real_dtype):
     return df, df_sinks
 
 
+def _create_mass_column(df, header_vars):
+    """
+    Creates a mass column with the mass of each particle when there are
+    multiple itypes.
+    """
+    df['mass'] = header_vars['massoftype']
+    for itype in df['itype'].unique():
+        if itype > 1:
+            df.loc[df.itype == itype, 'mass'] = header_vars[f'massoftype_{itype}']
+    return df
+
 def read_phantom(filename: str, separate_types: str = 'sinks', ignore_inactive: bool = True):
     """
     Read data from a Phantom dump file.
@@ -240,22 +251,34 @@ def read_phantom(filename: str, separate_types: str = 'sinks', ignore_inactive: 
         if ignore_inactive:
             df = df[df['h'] > 0]
 
-        if separate_types == 'all' and 'itype' in df and df['itype'].nunique() > 1:
-            df_list = []
-            for _, group in df.groupby('itype'):
-                itype = int(group["itype"].iloc[0])
-                mass_key = 'massoftype' if itype == 1 else f'massoftype_{itype}'
-                df_list.append(SarracenDataFrame(group.dropna(axis=1),
-                                                 params={**header_vars, **{"mass": header_vars[mass_key]}}))
+        # create mass column if multiple species in single dataframe
+        if separate_types != 'all' and 'itype' in df and df['itype'].nunique() > 1:
+            df = _create_mass_column(df, header_vars)
+        else:  # create global mass parameter
+            header_vars['mass'] = header_vars['massoftype']
+
+        df_list = []
+        if separate_types == 'all':
+            if 'itype' in df and df['itype'].nunique() > 1:
+                for _, group in df.groupby('itype'):
+                    itype = int(group["itype"].iloc[0])
+                    mass_key = 'massoftype' if itype == 1 else f'massoftype_{itype}'
+                    df_list.append(SarracenDataFrame(group.dropna(axis=1),
+                                                     params={**header_vars, **{"mass": header_vars[mass_key]}}))
+            else:
+                df_list = [SarracenDataFrame(df, params=header_vars)]
 
             if not df_sinks.empty:
                 df_list.append(SarracenDataFrame(df_sinks, params=header_vars))
 
-            return df_list
+        elif separate_types == 'sinks':
+            df_list = [SarracenDataFrame(df, params=header_vars)]
+            if not df_sinks.empty:
+                df_list.append(SarracenDataFrame(df_sinks, params=header_vars))
+        else:
+            df_list = [SarracenDataFrame(pd.concat([df, df_sinks], ignore_index=True),
+                                        params=header_vars)]
 
-        if (separate_types == 'sinks' or separate_types == 'all') and not df_sinks.empty:
-            return [SarracenDataFrame(df, params={**header_vars, **{"mass": header_vars['massoftype']}}),
-                    SarracenDataFrame(df_sinks, params=header_vars)]
+        df_list = df_list[0] if len(df_list) == 1 else df_list
 
-        return SarracenDataFrame(pd.concat([df, df_sinks], ignore_index=True),
-                                 params={**header_vars, **{"mass": header_vars['massoftype']}})
+        return df_list
