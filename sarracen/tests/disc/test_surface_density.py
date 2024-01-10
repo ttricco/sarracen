19b513e481a6b658327bb827c277d2bc796b5edd
Terrence Tricco <tstricco@mun.ca>
2024-01-10 00:22:47 -0330

    disc scale height and angular momenta calculations

--- sarracen/tests/disc/test_surface_density.py
+++ sarracen/tests/disc/test_surface_density.py
@@ -25,6 +25,7 @@ def test_mass_equivalency():
 
     assert_array_equal(sigma1, sigma2)
 
+
 @pytest.mark.parametrize("geometry", ['cylindrical', 'spherical'])
 def test_origin(geometry):
     """ Expect same profile regardless of origin. """
@@ -53,6 +54,29 @@ def test_origin(geometry):
     sigma_large = surface_density(sdf, origin=[5.6e4, -8.7e3, 5.4e6],
                                   bins=30, geometry=geometry)
 
-    assert_allclose(sigma_zero, sigma_eps, rtol=2e-7)
-    assert_allclose(sigma_zero, sigma_large, rtol=2e-7)
+    assert_allclose(sigma_zero, sigma_eps, atol=2e-8, rtol=0.0)
+    assert_allclose(sigma_zero, sigma_large, atol=2e-8, rtol=0.0)
 
+
+def test_parts_vs_whole():
+    """ Profiles should be the same for matching bins. """
+
+    # randomly place particles
+    rng = np.random.default_rng(seed=5)
+    x = rng.random(100)
+    y = rng.random(100)
+    z = rng.random(100)
+    vx = rng.random(100)
+    vy = rng.random(100)
+    vz = rng.random(100)
+    mass = [3.2e-4] * 100
+
+    sdf = SarracenDataFrame(data={'x': x, 'y': y, 'z': z,
+                                  'vx': vx, 'vy': vy, 'vz': vz,
+                                  'mass': mass})
+    sigma_in = surface_density(sdf, r_in=0.0, r_out=0.5, bins=100)
+    sigma_out = surface_density(sdf, r_in=0.5, r_out=1.0, bins=100)
+    sigma_all = surface_density(sdf, r_in=0.0, r_out=1.0, bins=200)
+
+    assert_allclose(sigma_in, sigma_all[:100], atol=1e-15, rtol=0.0)
+    assert_allclose(sigma_out, sigma_all[100:], atol=1e-15, rtol=0.0)
