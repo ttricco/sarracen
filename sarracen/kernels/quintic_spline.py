aa868b908d9631b15bf2be4b7162589d229faa46
Terrence Tricco <tstricco@mun.ca>
2024-07-18 12:57:25 -0230

    lint kernels

--- sarracen/kernels/quintic_spline.py
+++ sarracen/kernels/quintic_spline.py
@@ -14,10 +14,10 @@ class QuinticSplineKernel(BaseKernel):
     @staticmethod
     @njit(fastmath=True)
     def w(q: float, ndim: int):
-        norm = 1 / 120 if (ndim == 1) else \
-            7 / (478 * np.pi) if (ndim == 2) else \
-            1 / (120 * np.pi)
+        norm = 1 / 120 if (ndim == 1) \
+            else 7 / (478 * np.pi) if (ndim == 2) \
+            else 1 / (120 * np.pi)
 
-        return norm * ((3 - q) ** 5 * (q < 3)
-                       - 6 * (2 - q) ** 5 * (q < 2)
-                       + 15 * (1 - q) ** 5 * (q < 1)) * (0 <= q)
+        return norm * ((3 - q)**5 * (q < 3)
+                       - 6 * (2 - q)**5 * (q < 2)
+                       + 15 * (1 - q)**5 * (q < 1)) * (0 <= q)
