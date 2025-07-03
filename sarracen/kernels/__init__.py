27b4721166725e6708350a4b5994c782d89f5936
Terrence Tricco <tstricco@mun.ca>
2025-07-03 12:29:37 -0230

    define __all__ public API (squelching flake8 errors)

--- sarracen/kernels/__init__.py
+++ sarracen/kernels/__init__.py
@@ -2,3 +2,6 @@ from ..kernels.base_kernel import BaseKernel
 from ..kernels.cubic_spline import CubicSplineKernel
 from ..kernels.quartic_spline import QuarticSplineKernel
 from ..kernels.quintic_spline import QuinticSplineKernel
+
+__all__ = ["BaseKernel", "CubicSplineKernel", "QuarticSplineKernel",
+           "QuinticSplineKernel"]
