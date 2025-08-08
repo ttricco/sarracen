df17986f151f8dacd23a3543c85ab7bc92f7d8f0
Fathima Manooja <fmanooja@mun.ca>
2025-08-08 20:31:50 -0230

    test csv reader

--- sarracen/tests/readers/test_read_csv.py
+++ sarracen/tests/readers/test_read_csv.py
@@ -10,11 +10,11 @@ def test_get_units() -> None:
                             '[]'])
     test_units = read_csv._get_units(test_units)
 
-    answer_units = ['units /$:. ',
-                    'a',
-                    None,
-                    None]
-    answer_units = pd.Series(answer_units, name=0)
+    answer_units_list = ['units /$:. ',
+                         'a',
+                         None,
+                         None]
+    answer_units = pd.Series(answer_units_list, name=0)
 
     tm.assert_series_equal(test_units, answer_units)
 
@@ -26,10 +26,10 @@ def test_get_labels() -> None:
                              '[]'])
     test_labels = read_csv._get_labels(test_labels)
 
-    answer_labels = ['va_r?s w/ sp3ci@l ch%rs',
-                     'a',
-                     'a',
-                     None]
-    answer_labels = pd.Series(answer_labels, name=0)
+    answer_labels_list = ['va_r?s w/ sp3ci@l ch%rs',
+                          'a',
+                          'a',
+                          None]
+    answer_labels = pd.Series(answer_labels_list, name=0)
 
     tm.assert_series_equal(test_labels, answer_labels)
