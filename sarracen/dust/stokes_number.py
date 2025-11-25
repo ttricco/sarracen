da04fc2fb49b8e2447d539d7e9c87a791657e7b6
Fathima <iamfathima293@gmail.com>
2025-11-25 18:25:42 -0330

    stokes in new folder

---
+++ sarracen/dust/stokes_number.py
@@ -0,0 +1,187 @@
+import numpy as np
+import pandas as pd
+from scipy.interpolate import griddata
+from sklearn.neighbors import KDTree
+from ..sarracen_dataframe import SarracenDataFrame
+
+from typing import Union
+
+
+# BRANCH CODE BELOW
+
+#SELF IMPLIES SARRACEN DATAFRAME. NEED TO UPDATE THIS!!!!!!
+
+#method to interpolate gas density for dust particles
+def interpolate_gas_density(self):
+    """Linearly interpolates gas density onto dust particles using a grid method.
+    """
+    if "rho_gas" not in self: # speed up by only performing interpolation if not already done
+
+        self.calc_density()
+        self["rho_gas"] = self["rho"]
+
+        gas_df = self[self["itype"]==1]
+        gas_pos = (gas_df["x"],gas_df["y"],gas_df["z"])
+        gasvals = gas_df["rho"]
+
+        dust_df = self[self["itype"]==7]
+        dust_pos = (dust_df["x"],dust_df["y"],dust_df["z"])
+        dustvals = griddata(gas_pos,gasvals,dust_pos,method="nearest")
+
+        self.loc[self["itype"]==7,"rho_gas"] = dustvals
+
+def calc_stokes(self,c_s,G=1):
+    """"Calculates Stokes number of two-fluid dust particles."""
+    if "St" not in self:
+        self.interpolate_gas_density()
+        self["r"] = (self[self.xcol]**2 + self[self.ycol]**2)**0.5
+        omega_kep = (self.sinks["m"][0]*G/self["r"]**3)**0.5
+        t_s = self.params["graindens"] * self.params["grainsize"]/(self["rho_gas"]+self["rho"])/c_s
+        self["St"] = t_s * omega_kep
+        self.loc[self["itype"]==1,"St"] = 0
+
+#---------------------------------------------------------------------------------------------------------------
+# PR CODE BELOW
+
+def _default_vxyz(data, vx, vy, vz):
+    """
+    Utility function to determine the vx, vy, and vz columns to use.
+    Parameters
+    ----------
+    data: SarracenDataFrame
+        The particle dataset to use.
+    vx, vy, vz: str
+        The vx, vy, and vz directional column labels.
+    Returns
+    -------
+    vx, vy, vz: str
+        The directional column labels to use.
+    """
+    if vx is None:
+        vx = data.vxcol if not vy == data.vxcol and not vz == data.vxcol else \
+             data.vycol if not vy == data.vycol and not vz == data.vycol else data.vzcol
+    if vy is None:
+        vy = data.vycol if not vx == data.vycol and not vz == data.vycol else \
+             data.vxcol if not vx == data.vxcol and not vz == data.vxcol else data.vzcol
+    if vz is None:
+        vz = data.vzcol if not vx == data.vzcol and not vy == data.vzcol else \
+             data.vycol if not vx == data.vycol and not vy == data.vycol else data.vxcol
+
+    return vx, vy, vz
+
+def stoppingtime(rho_dust, rho_gas, v_gas, v_dust, rho_grain, sgrain, gamma, c_s):
+    return np.sqrt(np.pi * gamma * 0.125) * rho_grain * sgrain / (rho_dust + rho_gas) / np.sqrt(1 + 0.0703125 * np.pi * (np.linalg.norm(v_gas - v_dust))**2 / c_s**2)
+
+def Stokes_number(data_dust: 'SarracenDataFrame',
+                  data_gas: 'SarracenDataFrame',
+                  rho_grid: float,
+                  sgrain: float,
+                  c_s: float,
+                  kernel: BaseKernel = None,
+                  x_dust: str = None,
+                  y_dust: str = None,
+                  z_dust: str = None,
+                  vx_dust: str = None,
+                  vy_dust: str = None,
+                  vz_dust: str = None,
+                  x_gas: str = None,
+                  y_gas: str = None,
+                  z_gas: str = None,
+                  vx_gas: str = None,
+                  vy_gas: str = None,
+                  vz_gas: str = None,
+                  backend: str = None):
+
+    _check_dimension(data_dust, 3)
+    _check_dimension(data_gas, 3)
+
+    x_dust, y_dust, z_dust = _default_xyz(data_dust, x_dust, y_dust, z_dust)
+    x_gas, y_gas, z_gas = _default_xyz(data_gas, x_gas, y_gas, z_gas)
+    vx_dust, vy_dust, vz_dust = _default_vxyz(data_dust, vx_dust, vy_dust, vz_dust)
+    vx_gas, vy_gas, vz_gas = _default_vxyz(data_gas, vx_gas, vy_gas, vz_gas)
+
+    _verify_columns(data_dust, [x_dust, y_dust, z_dust, 'h', vx_dust, vy_dust, vz_dust])
+    _verify_columns(data_gas, [x_gas, y_gas, z_gas, 'h', vx_gas, vy_gas, vz_gas])
+
+    x_dust_data = data_dust[x_dust].values
+    y_dust_data = data_dust[y_dust].values
+    z_dust_data = data_dust[z_dust].values
+    h_dust_data = data_dust['h'].values
+    vx_dust_data = data_dust[vx_dust].values
+    vy_dust_data = data_dust[vy_dust].values
+    vz_dust_data = data_dust[vz_dust].values
+    rho_dust_data = _get_density(data_dust)
+
+    x_gas_data = data_gas[x_gas].values
+    y_gas_data = data_gas[y_gas].values
+    z_gas_data = data_gas[z_gas].values
+    h_gas_data = data_gas['h'].values
+    vx_gas_data = data_gas[vx_gas].values
+    vy_gas_data = data_gas[vy_gas].values
+    vz_gas_data = data_gas[vz_gas].values
+    rho_gas_data = _get_density(data_gas)
+
+    # Combine coordinates
+    gas_positions = np.vstack((x_gas_data, y_gas_data, z_gas_data)).T
+    dust_positions = np.vstack((x_dust_data, y_dust_data, z_dust_data)).T
+
+    gas_velocity = np.vstack((vx_gas_data, vy_gas_data, vz_gas_data)).T
+    dust_velocity = np.vstack((vx_dust_data, vy_dust_data, vz_dust_data)).T
+
+    tree = KDTree(gas_positions, leaf_size=10)
+
+    # Search using gas particle positions and smoothing lengths
+    neighbours = tree.query_radius(dust_positions, r=2 * h_gas_data)
+
+    # Initialize structure to hold gas neighbours of each dust particle
+    dust_neighbours = [np.array([], dtype=np.int64) for _ in range(len(dust_positions))]
+
+    # Loop over each gas-dust neighbour pair
+    for gas_particle, gas_neighbours in enumerate(neighbours):
+        for dust_neighbour in gas_neighbours:
+            dust_neighbours[dust_neighbour] = np.append(dust_neighbours[dust_neighbour], gas_particle)
+
+    dust_number = len(x_dust_data)
+    dim = 3
+    rhog_on_dust = np.zeros(dust_number)
+    vx_on_dust = np.zeros(dust_number)
+    vy_on_dust = np.zeros(dust_number)
+    vz_on_dust = np.zeros(dust_number)
+
+    for ind, array in enumerate(dust_neighbours):
+        vx_neighb = 0
+        vy_neighb = 0
+        vz_neighb = 0    
+        neighbor_rho = 0
+
+        rho_dust = rho_dust_data[ind]
+        r_dust = dust_positions[ind]
+        v_dust = dust_velocity[ind]
+
+        for j in array:
+            rho_gas = rho_gas_data[j]
+            r_gas = gas_positions[j]
+            h_gas = h_gas_data[j]
+            vx_gas = vx_gas_data[j]
+            vy_gas = vy_gas_data[j]
+            vz_gas = vz_gas_data[j]
+
+            q = np.linalg.norm(r_gas - r_dust) / h_gas
+            normalized_weight = kernel.w(q, dim)
+
+            neighbor_rho += rho_gas * normalized_weight
+            vx_neighb += vx_gas * normalized_weight
+            vy_neighb += vy_gas * normalized_weight
+            vz_neighb += vz_gas * normalized_weight
+
+        rhog_on_dust[ind] = neighbor_rho 
+        vx_on_dust[ind] = vx_neighb
+        vy_on_dust[ind] = vy_neighb
+        vz_on_dust[ind] = vz_neighb
+
+    gas_velocity_on_dust = np.vstack((vx_on_dust, vy_on_dust, vz_on_dust)).T
+
+    tstop = stoppingtime(rho_dust_data, rhog_on_dust, gas_velocity_on_dust, dust_velocity, rho_grain, sgrain, gamma, c_s)
+    stokes_number = tstop * c_s * rhog_on_dust * (1/3) / data_gas.params['hfact'] * data_gas.params['mass']**(1/3)
+
+    return stokes_number
