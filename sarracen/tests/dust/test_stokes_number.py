f9e17387b93ab9b431a8b7841f27ef53ed1e2c0b
Fathima Manooja <fmanooja@mun.ca>
2025-12-08 15:55:02 -0330

    unit test fir inverting locations

--- sarracen/tests/dust/test_stokes_number.py
+++ sarracen/tests/dust/test_stokes_number.py
@@ -32,7 +32,7 @@ def test_getting_dust_locations():
     assert dust_locations2[1][0] == 0
     assert dust_locations2[2][0] == 1
 
-def test_getting_dust_locations_diff_length():
+def test_getting_dust_locations_diff_size():
     datag = {'x': [2, 8, 3], 'y': [8, 5, 2], 'h': [0.5, 0.5, 1]}
     datad = {'x': [2, 4, 9, 2.1], 'y': [2, 8, 5, 2], 'h': [0.25, 1.5, 1, 0.2]}
     sdfg = SarracenDataFrame(datag, params=dict())
@@ -60,3 +60,64 @@ def test_getting_dust_locations_diff_length():
     assert dust_locations2[1][0] == 0
     assert dust_locations2[2][0] == 1
 
+def test_inverting_dust_locations():
+    datag = {'x': [2, 8, 3], 'y': [8, 5, 2], 'h': [0.5, 0.5, 1]}
+    datad = {'x': [2, 4, 9], 'y': [2, 8, 5], 'h': [0.25, 1.5, 1]}
+    sdfg = SarracenDataFrame(datag, params=dict())
+    sdfd = SarracenDataFrame(datad, params=dict())
+    gas_positions = sdfg[["x", "y"]].values
+    dust_positions = sdfd[["x", "y"]].values
+    h_gas_data = sdfg["h"].values
+    h_dust_data = sdfd["h"].values
+
+    # The result from getting_dust_locations(dust_positions, gas_positions, h_gas_data)
+    dust_locations1 = [[], [2], [0]]
+    dust_locations1_inverse = inverting_dust_locations(dust_positions, dust_locations1)
+    assert len(dust_locations1_inverse) == len(sdfd)
+    assert len(dust_locations1_inverse[0]) == 1
+    assert len(dust_locations1_inverse[1]) == 0
+    assert len(dust_locations1_inverse[2]) == 1
+    assert dust_locations1_inverse[0][0] == 2
+    assert dust_locations1_inverse[2][0] == 1
+
+    # The result from getting_dust_locations(gas_positions, dust_positions, h_dust_data)
+    dust_locations2 = [[], [0], [1]]
+    dust_locations2_inverse = inverting_dust_locations(gas_positions, dust_locations2)
+    assert len(dust_locations2_inverse) == len(sdfg)
+    assert len(dust_locations2_inverse[0]) == 1
+    assert len(dust_locations2_inverse[1]) == 1
+    assert len(dust_locations2_inverse[2]) == 0
+    assert dust_locations2_inverse[0][0] == 1
+    assert dust_locations2_inverse[1][0] == 2
+
+def test_inverting_dust_locations_diff_size():
+    datag = {'x': [2, 8, 3], 'y': [8, 5, 2], 'h': [0.5, 0.5, 1]}
+    datad = {'x': [2, 4, 9, 2.1], 'y': [2, 8, 5, 2], 'h': [0.25, 1.5, 1, 0.2]}
+    sdfg = SarracenDataFrame(datag, params=dict())
+    sdfd = SarracenDataFrame(datad, params=dict())
+    gas_positions = sdfg[["x", "y"]].values
+    dust_positions = sdfd[["x", "y"]].values
+    h_gas_data = sdfg["h"].values
+    h_dust_data = sdfd["h"].values
+
+    # The result from getting_dust_locations(dust_positions, gas_positions, h_gas_data)
+    dust_locations1 = [[], [2], [0, 3]]
+    dust_locations1_inverse = inverting_dust_locations(dust_positions, dust_locations1)
+    assert len(dust_locations1_inverse) == len(sdfd)
+    assert len(dust_locations1_inverse[0]) == 1
+    assert len(dust_locations1_inverse[1]) == 0
+    assert len(dust_locations1_inverse[2]) == 1
+    assert len(dust_locations1_inverse[3]) == 1
+    assert dust_locations1_inverse[0][0] == 2
+    assert dust_locations1_inverse[2][0] == 1
+    assert dust_locations1_inverse[3][0] == 2
+
+    # The result from getting_dust_locations(gas_positions, dust_positions, h_dust_data)
+    dust_locations2 = [[], [0], [1], []]
+    dust_locations2_inverse = inverting_dust_locations(gas_positions, dust_locations2)
+    assert len(dust_locations2_inverse) == len(sdfg)
+    assert len(dust_locations2_inverse[0]) == 1
+    assert len(dust_locations2_inverse[1]) == 1
+    assert len(dust_locations2_inverse[2]) == 0
+    assert dust_locations2_inverse[0][0] == 1
+    assert dust_locations2_inverse[1][0] == 2
